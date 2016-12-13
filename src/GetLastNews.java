1fbf227daf389a6d0a7c5937aa6ad36d8f21a3bc
Flerry <nuryelse@gmail.com>
2016-12-13 20:04:10 +0300

    Fix article link

--- src/GetLastNews.java
+++ src/GetLastNews.java
@@ -8,26 +8,35 @@ import java.io.IOException;
 class GetLastNews {
     public static void getLastNews() {
         TgBot msgBridge = new TgBot();
+        String urlMainPage = "https://dev.by";
         String url = "https://dev.by/lenta";
+        Document lentaPage;
         Document mainPage;
         try {
-            mainPage = Jsoup.connect(url).timeout(30000).get();
+            lentaPage = Jsoup.connect(url).timeout(30000).get();
+            mainPage = Jsoup.connect(urlMainPage).timeout(30000).get();
+
+            Element itemRateLinear = lentaPage.select("div[class=item-rate_linear]").first();
+
+            Element itemTitleLinear = lentaPage.select("h3[class=item-title_linear]").first();
 
 
-            Element itemRateLinear = mainPage.select("div[class=item-rate_linear]").first();
+            Element articlePreviewTitle = mainPage.select("h2[class=article-preview__title]").first();
 
-            Element itemTitleLinear = mainPage.select("h3[class=item-title_linear]").first();
+            Elements elementPublicLink = articlePreviewTitle.select("a[href]");
 
-            Elements itemRateLinks = itemRateLinear.select("a[href]");
+            String publicLink = elementPublicLink.attr("abs:href");
 
 
-            String articleLink = itemRateLinks.attr("abs:href");
             String title = itemTitleLinear.text();
 
-            msgBridge.sendMsgCustomUser(TgBot.chatId, title + "\n" + articleLink);
+            msgBridge.sendMsgCustomUser(TgBot.chatId, title + "\n" + publicLink);
+
 
         } catch (IOException e) {
             msgBridge.sendMsgCustomUser(TgBot.chatId, "Извините, сайт не ответил на запрос...");
         }
     }
 }
+
+
